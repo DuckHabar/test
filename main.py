a646313bce0ffbd10efde643d444cd2f78da2324
vladsn7821 <vladislav.sn.mgn@gmail.com>
2022-12-02 19:00:49 +0500

    New day — new print

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
+def main():
     print('My first git program')
-    print('And I change it every day')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
